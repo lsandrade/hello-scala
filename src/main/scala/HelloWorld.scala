72fe65bfca9fe50f99e8d9c9a548c2270c3ecde3
Luan Andrade <luansandrade92@hotmail.com>
2020-05-18 15:48:50 -0300

    format date

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,12 @@
+import java.util.{Date, Locale}
+import java.text.DateFormat._
+
 object HelloWorld {
     def main(args: Array[String]): Unit = {
-      print("Hello World")
+      val now = new Date
+      val df = getDateInstance(LONG, Locale.ENGLISH)
+      println("Hello World")
+
+      println(df format now)
     }
 }
