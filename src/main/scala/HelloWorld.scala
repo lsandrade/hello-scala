14ce8f81c91d03430f7b587fc1bb04bb0f87c00f
Luan Andrade <luansandrade92@hotmail.com>
2020-05-18 15:55:15 -0300

    anonymous function

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -2,13 +2,24 @@ import java.util.{Date, Locale}
 import java.text.DateFormat._
 
 object HelloWorld {
+    def oncePerSecond(callback: () => Unit): Unit = {
+      while (true) { callback(); Thread sleep 1000}
+    }
+
     def main(args: Array[String]): Unit = {
       val now = new Date
       val df = getDateInstance(LONG, Locale.ENGLISH)
+
+      // Hello world
       println("Hello World")
 
+      // Format date
       println(df format now)
 
+      // sum values
       println( 1 + 3)
+
+      // call anonymous function
+      oncePerSecond(() => println("time flies like an arrow..."))
     }
 }
